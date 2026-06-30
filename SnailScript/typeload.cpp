dcf4bcc40aa7e0ac6f2a4f063287db24f72da5d4
Infosoft Corporation <mishra.anay@icloud.com>
2026-06-30 17:09:35 +0200

    Create typeload.cpp

---
+++ SnailScript/typeload.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <raylib.h>
+#include <iostream>
+using namespace std;
+
+void useingsysten()
+{
+    WINCLASS wc;
+    wc.loaderindexed              = LoadIndexing(nullptr);
+    if (!msg) { return wc.loaderindexed; };
+};
+
+// end 
