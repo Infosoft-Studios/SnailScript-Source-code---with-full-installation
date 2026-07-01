861c05779fb62a79613ef8585dc5fa9a26ad8261
Infosoft Corporation <mishra.anay@icloud.com>
2026-07-01 21:25:25 +0200

    Create WindowsAPIreinstaller[1].cpp

---
+++ SnailScript/WindowsAPIreinstaller[1].cpp
@@ -0,0 +1,104 @@
+#include <windows.h>
+#include <raylib.h>
+#include <iostream>
+using namespace std;
+
+void returnsystemvoid()
+{
+    WINCLASS wc;
+    wc.loadedindexed            = indexed(nullptr, true);
+    /*load pointer
+    Infosoft
+    */
+    
+    wc.loadpointer              = IndexedPointerw(900, nullptr);
+    
+    switch (wc.getLoader)
+    {
+        case 1:
+            wc.loaderinit               = IndexedPointerMemory(nullptr);
+            wc.redoinit                 = InternetReproxy("type", nullptr);
+        default:
+            wc.loaderreporxy            = wc.drawpx(nullptr, 900);
+    };
+};
+class type_loader
+{
+    // loadRGB
+    public:
+        virtual void functionnline()
+        {
+            wc.pistonsValue         = GetFromRGB(255, 90, 34); // gets value from RGB 
+            wc.getRGBdata           = LibaryRG(nullptr, 90);
+        };
+}
+
+class recallsystem : public type_loader
+{
+    // Override system
+    public:
+        void functionnline(int func) { std::cout << func << std::endl; };
+        wc.loaderinit               = loadpxn(nullptr, 90);
+        wc.getstyleparts            = InitWindow(900, 800, "Tye N Line");
+        wc.loaderreporxy            = RedoInit(nullptr, 90);
+    private:
+        wc.nload32                  = ReTypeInit(nullptr, true;
+        switch (!msg)
+        {
+            wc.partinit             = NeType(nullptr, 90);
+            wc.redointfunction      = ReturnType(nullptr);
+        };
+        switch(!wc.IndexedPointerMemory)
+        {
+            case 1:
+                wc.loaderpointedmemory              = FunctionAssets(nullptr, 9);
+                std::cout << "functional assets has nullptr in the first argument";
+            default:
+                return 0;
+        }
+    protected: // as main
+        void __main__()
+        {
+             wc.loadedfunctionalassets                  = RedoFunctionMainsOfINT(nullptr, NULL);
+             wc.redofunctionalassertsrgb                = LoadFromRGB(90, nullptr);
+             wc.getwindowstatuspoints                   = InitWindow(900, 890, "StatusPoints - C++");
+             if (!wc.redofunctionalassertsrgb)
+             {
+                return wc.loaderpointedmemory;             
+             };
+             
+             wc.getparticalsystem                       = LoaderInitializer(nullptr);
+             return 0;
+        };
+        void __int__main()
+        {
+            wc.loadedinitlailizer                       = LoadFunctionalAsst(nullptr, 90);
+            wc.getendmainpoint                          = LoadFunctions(100, 100);
+            switch (!wc.getloaderdatapoint)
+            {
+                case 1:
+                    wc.loadedinitpsi                    = GetPointFromRGB(nullptr, 90-68); // TUFF MATHEMATICAL QUESTIO, WOWOWOOW
+                    wc.objectorientedsystem             = LoaderFunction32("Rewrite%$", 90*9-1);
+                case 2:
+                    wc.getfunctionalloaders             = RelodePartType(nullptr, 67);
+                    wc.LoaderInitializer                = Refunctionize(nullptr, 90,
+                    "loadertype",
+                    nullptr
+                    );
+                    
+                    return wc.loaderfunction32;
+                    
+            }
+            }
+        }
+};
+
+int main()
+{
+    recallsystem obj;
+    obj.functionnline(90);
+};
+
+/*
+infosoft codes // done*
+*/
